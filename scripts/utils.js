cd32311e50c96e03541f33c4766256b7045c84dd
madjarevicn <nikola.madjarevic@gmail.com>
2021-04-09 08:47:46 +0200

    Add utils.js script.

---
+++ scripts/utils.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+
+function getSavedContractAddresses() {
+    let json
+    try {
+        json = fs.readFileSync(path.join(__dirname, `../contract-addresses.json`))
+    } catch (err) {
+        json = '{}'
+    }
+    return JSON.parse(json)
+}
+
+function saveContractAddress(network, contract, address) {
+    const addrs = getSavedContractAddresses()
+    addrs[network] = addrs[network] || {}
+    addrs[network][contract] = address
+    fs.writeFileSync(path.join(__dirname, `../contract-addresses.json`), JSON.stringify(addrs, null, '    '))
+}
+
+module.exports = {
+    getSavedContractAddresses,
+    saveContractAddress
+}
