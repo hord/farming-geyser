8f8751a2336885437bb52476358d7da48711fc45
madjarevicn <nikola.madjarevic@gmail.com>
2021-04-13 16:40:56 +0200

    Align testnet-deploy.js script

--- scripts/testnet-deploy.js
+++ scripts/testnet-deploy.js
@@ -4,6 +4,7 @@ const { getSavedContractAddresses, saveContractAddress } = require('./utils')
 async function main() {
 
   const hord_lp_token = '0xb894fcc9e152e1a6d4272bcbd958429b9d5f1737';
+  const hord_token = '0xD90F43c95f213FF09D3a3070bfd78B46Dfe5E9de';
 
   const IERC20 = await hre.ethers.getContractFactory('IERC20');
   const Farm = await hre.ethers.getContractFactory('Farm');
@@ -17,7 +18,13 @@ async function main() {
   saveContractAddress(hre.network.name, 'farm', farm.address, (await hre.artifacts.readArtifact("Farm")).abi);
 
   let totalRewards = "100800000000000000000000" // 10 days approximately
-  await IERC20(hord_lp_token).approve(farm.address, totalRewards);
+  console.log('Approving the farm to take rewards token');
+  await IERC20(hord_token).approve(farm.address, totalRewards);
+  console.log('Funding the farm with rewards token');
+  await farm.fund(totalRewards);
+  console.log('Create new farming pool for hord lp token');
+  await farm.add(100, hord_lp_token, true);
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
