3e8e5d569cee3700f52223f7b5ac6af43ecfc49a
madjarevicn <nikola.madjarevic@gmail.com>
2021-04-21 11:58:36 +0200

    Save ABIs and crate new folder.

--- scripts/utils.js
+++ scripts/utils.js
@@ -4,26 +4,46 @@ const path = require('path')
 function getSavedContractAddresses() {
     let json
     try {
-        json = fs.readFileSync(path.join(__dirname, `../contract-addresses.json`))
+        json = fs.readFileSync(path.join(__dirname, `../deployments/contract-addresses.json`))
     } catch (err) {
         json = '{}'
     }
     return JSON.parse(json)
 }
 
-function saveContractAddress(network, contract, address, abi) {
+function saveContractAddress(network, contract, address) {
     const addrs = getSavedContractAddresses()
     addrs[network] = addrs[network] || {}
-    const config = {
-        'address' : address,
-        'abi' : abi
-    }
-    addrs[network][contract] = config;
-    fs.writeFileSync(path.join(__dirname, `../contract-addresses.json`), JSON.stringify(addrs, null, '    '))
+    addrs[network][contract] = address
+    fs.writeFileSync(path.join(__dirname, `../deployments/contract-addresses.json`), JSON.stringify(addrs, null, '    '))
 }
 
+function getSavedContractAbis(env) {
+    if(!env) {
+        env = 'local'
+    }
+    let json
+    try {
+        json = fs.readFileSync(path.join(__dirname, `../deployments/contract-abis.json`))
+    } catch (err) {
+        json = '{}'
+    }
+    return JSON.parse(json[env])
+}
+
+function saveContractAbis(network, contract, bytecode, env) {
+    if(!env) {
+        env = 'local'
+    }
+    const bytecodes = getSavedContractAbis()
+    bytecodes[network] = bytecodes[network] || {}
+    bytecodes[network][contract] = bytecode
+    fs.writeFileSync(path.join(__dirname, `../deployments/contract-abis.json`), JSON.stringify(bytecodes, null, '    '))
+}
 
 module.exports = {
     getSavedContractAddresses,
-    saveContractAddress
+    saveContractAddress,
+    getSavedContractAbis,
+    saveContractAbis
 }
