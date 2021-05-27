1f9db9eeb2902ac07e6dc81d83bd0e6c9f7e2803
markolazic21 <marko@2key.network>
2021-05-27 12:39:50 +0200

    [add-tests] -> Deployments

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,15 +11,16 @@ async function main() {
     const rewardPerBlock = ethers.utils.parseEther("1.25"); // 1 token per block
 
     const Farm = await hre.ethers.getContractFactory('Farm');
-    const farm = await Farm.deploy(contracts["HordToken"], rewardPerBlock, currentBlock);
+    const farm = await Farm.deploy(contracts["RewardsToken"], rewardPerBlock, currentBlock);
     await farm.deployed();
     console.log('Farm deployed with address: ', farm.address);
     saveContractAddress(hre.network.name, 'Farm', farm.address);
 
     await farm.addPool(100, contracts["LpToken"], true);
 
-    let totalRewards = toWeiDenomination(100800)            // 10 days approximately
-    await hord_token.approve(farm.address, totalRewards);
+    let totalRewards = toWeiDenomination(100800)// 10 days approximately
+    const rewardsToken = ethers.getContractAt("ERC20Mock", contracts["RewardsToken"]);
+    await rewardsToken.approve(farm.address, totalRewards);
     console.log('Approved rewards token');
 
     console.log('Create new farming pool for hord lp token');
