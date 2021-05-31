bbe4425feeabff2e2045fdca6cfdbea5c5d04e13
madjarevicn <nikola.madjarevic@gmail.com>
2021-05-31 12:10:29 +0200

    Add deployment configuration for FEAR farm.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,26 +5,24 @@ const { ethers, web3, upgrades } = hre
 async function main() {
     const contracts = getSavedContractAddresses()[hre.network.name];
 
-    let currentBlock = await web3.eth.getBlockNumber();
-    currentBlock += 100;
-    console.log('startBlock: ' + currentBlock);
-    const rewardPerBlock = ethers.utils.parseEther("1.25"); // 1 token per block
+    const startBlock = 12542575; // 2 pm UTC 31st May 2021
+    const rewardPerBlock = ethers.utils.parseEther("1.003087"); // 1.003087 token per block
 
     const Farm = await hre.ethers.getContractFactory('Farm');
-    const farm = await Farm.deploy(contracts["RewardsToken"], rewardPerBlock, currentBlock);
+    const farm = await Farm.deploy(contracts["RewardsToken"], rewardPerBlock, startBlock);
     await farm.deployed();
     console.log('Farm deployed with address: ', farm.address);
     saveContractAddress(hre.network.name, 'Farm', farm.address);
 
-    await farm.addPool(100, contracts["LpToken"], true);
-
-    let totalRewards = ethers.utils.parseEther("100800")// 10 days approximately
-    const rewardsToken = await hre.ethers.getContractAt("ERC20Mock", contracts["RewardsToken"]);
-    await rewardsToken.approve(farm.address, totalRewards);
-    console.log('Approved rewards token');
-
-    console.log('Create new farming pool for hord lp token');
-    await farm.fund(totalRewards);
+    // await farm.addPool(100, contracts["LpToken"], true);
+    //
+    // let totalRewards = ethers.utils.parseEther("100800")// 10 days approximately
+    // const rewardsToken = await hre.ethers.getContractAt("ERC20Mock", contracts["RewardsToken"]);
+    // await rewardsToken.approve(farm.address, totalRewards);
+    // console.log('Approved rewards token');
+    //
+    // console.log('Create new farming pool for hord lp token');
+    // await farm.fund(totalRewards);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
