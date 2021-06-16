1676e4597e8a5fe535e5986f5cad02de6154d628
madjarevicn <nikola.madjarevic@gmail.com>
2021-06-16 20:41:01 +0200

    Add final deployment script.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,26 +5,38 @@ const { ethers, web3, upgrades } = hre
 async function main() {
     const contracts = getSavedContractAddresses()[hre.network.name];
 
-    const startBlock = 12645600; // 2 pm UTC 7th June 2021
-    const rewardPerBlock = ethers.utils.parseEther("0.2344"); // 0.2344 token per block
+    // Load rewards token
+    const rewardsToken = await hre.ethers.getContractAt("ERC20Mock", contracts["RewardsToken"]);
 
+
+    // Compute contract parameters
+    const startBlock = 12645600; // 2 pm UTC 7th June 2021
+    const tokenDecimals = await rewardsToken.decimals();
+    const rewardPerBlock = ethers.utils.parseUnits("0.2344", tokenDecimals.toString()); // 0.2344 token per block
+    const totalRewards = ethers.utils.parseUnits("200000", tokenDecimals.toString());
+
+
+    // Deploy Farm contract
     const Farm = await hre.ethers.getContractFactory('Farm');
     const farm = await Farm.deploy(contracts["RewardsToken"], rewardPerBlock, startBlock);
     await farm.deployed();
-
     console.log('Farm deployed with address: ', farm.address);
     saveContractAddress(hre.network.name, 'Farm', farm.address);
 
+
+    // Add pool with 100 allocation points, enforce update
     await farm.addPool(100, contracts["LpToken"], true);
 
-    let totalRewards = ethers.utils.parseEther("200000");
-    const rewardsToken = await hre.ethers.getContractAt("ERC20Mock", contracts["RewardsToken"]);
+
+    // Approve rewards token so FARM can enforce transferFrom call from user
     await rewardsToken.approve(contracts['Farm'], totalRewards);
     console.log('Approved rewards token');
 
-    // console.log('Create new farming pool for hord lp token');
-    // await farm.fund(totalRewards);
-    // console.log('Farm funded properly.');
+
+    // Fund the farm, send the tokens to the farm
+    await farm.fund(totalRewards);
+    const endBlock = await farm.endBlock();
+    console.log('Farm is funded properly. Farm starts at block: ', startBlock, ' and ends at: ', endBlock);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
